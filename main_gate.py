60dd467b4a5471aa4680c83a1b7a2b9a02a66cd3
ENbanned <your_email@example.com>
2025-10-21 23:15:01 +0200

    auto update: 2025-10-21 23:15:01

--- main_gate.py
+++ main_gate.py
@@ -9,17 +9,27 @@ async def main():
     # methods = [m for m in dir(GateClient) if callable(getattr(GateClient, m))]
     # print(methods)
 
+    print('-' * 20)
+    
     result_1 = gate.get_price('ENA_USDT')
     print(result_1)
 
-    result_2 = ...
+    print('-' * 20)
+
+    result_2 = await gate.buy_market('ENA_USDT', 100)
     print(result_2)
 
-    result_3 = ...
+    print('-' * 20)
+
+    result_3 = await gate.get_positions()
     print(result_3)
 
-    result_4 = ...
+    print('-' * 20)
+
+    result_4 = await gate.sell_market('ENA_USDT', 100)
     print(result_4)
+
+    print('-' * 20)
 
 
 
