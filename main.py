7bae249994f5f6bb2a19116d1f436d7831df00c8
ENbanned <your_email@example.com>
2025-10-22 17:25:01 +0200

    auto update: 2025-10-22 17:25:01

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.exchanges.hyperliquid.client import HyperliquidClient
 async def main():
   async with HyperliquidClient(HYPERLIQUID_SECRET_KEY, HYPERLIQUID_ACCOUNT_ADDRESS) as hl:
     print(123)
-    result = hl.get_sz_decimals('BTC')
+    result = await hl.buy_market('ENA', 1000000)
 
     print(result)
     
--- main_gate.py
+++ main_gate.py
@@ -7,7 +7,7 @@ from src.exchanges.gate.client import GateClient
 async def main():
   async with GateClient(GATE_API_KEY, GATE_API_SECRET) as gate:
     
-    result = await gate.buy_market('XPL_USDT', 100)
+    result = await gate.sell_market('XPL_USDT', 100)
     print(result)
 
 
