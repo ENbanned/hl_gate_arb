a13acfc010c3de2eb40732ba2d20443cbb3b3a64
ENbanned <your_email@example.com>
2025-10-22 17:45:01 +0200

    auto update: 2025-10-22 17:45:01

--- main.py
+++ main.py
@@ -15,7 +15,39 @@ async def main():
       gate: ExchangeClient = gate_client
       hyperliquid: ExchangeClient = hyperliquid_client
 
-      open_gate = await gate.buy_market
+      gate_open_positions = await gate.get_positions()
+      hyperliquid_open_positions = await hyperliquid.get_positions()
+
+      print(f'Gate positions before: {gate_open_positions}')
+      print(f'Hyperliquid positions before: {hyperliquid_open_positions}')
+
+      open_gate = await gate.buy_market('ENA', 100)
+      print(f'GATE LONG 100 ENA: {open_gate}')
+
+      open_hyperliquid = await hyperliquid.buy_market('ENA', 100)
+      print(f'HYPERLIQUID LONG 100 ENA: {open_hyperliquid}')
+
+      gate_open_positions = await gate.get_positions()
+      hyperliquid_open_positions = await hyperliquid.get_positions()
+
+      print(f'Gate positions after: {gate_open_positions}')
+      print(f'Hyperliquid positions after: {hyperliquid_open_positions}')
+
+      gate_close = await gate.sell_market('ENA', 100)
+      print(f'GATE CLOSE LONG 100 ENA: {gate_close}')
+
+      hyperliquid_close = await hyperliquid.sell_market('ENA', 100)
+      print(f'HYPERLIQUID CLOSE LONG 100 ENA: {hyperliquid_close}')
+
+      gate_open_positions = await gate.get_positions()
+      hyperliquid_open_positions = await hyperliquid.get_positions()
+
+      print(f'Gate positions at the end: {gate_open_positions}')
+      print(f'Hyperliquid positions at the end: {hyperliquid_open_positions}')
+
+
+
+
 
 
 asyncio.run(main())
