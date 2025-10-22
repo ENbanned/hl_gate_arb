785cf2fc1a3196c8f73fd1a22d97e74609c48e06
ENbanned <your_email@example.com>
2025-10-22 17:40:01 +0200

    auto update: 2025-10-22 17:40:01

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from src.exchanges.common import ExchangeClient, Position
 from src.exchanges.gate import GateClient
 from src.exchanges.hyperliquid import HyperliquidClient
@@ -7,14 +9,13 @@ from settings import GATE_API_KEY, GATE_API_SECRET, HYPERLIQUID_ACCOUNT_ADDRESS,
 
 
 async def main():
-  async with GateClient(GATE_API_KEY, GATE_API_SECRET) as gate:
-    async with HyperliquidClient(HYPERLIQUID_SECRET_KEY, HYPERLIQUID_ACCOUNT_ADDRESS) as hl:
-      clients: list[ExchangeClient] = [gate, hl]
-      
-      for client in clients:
-        positions = await client.get_positions()
-        for pos in positions:
-          print(f"{pos.coin}: {pos.side} {pos.size} @ {pos.entry_price}")
-        
-        order = await client.buy_market('ENA', 100)
-        print(f"Order {order.order_id}: {order.status}")
+  async with GateClient(GATE_API_KEY, GATE_API_SECRET) as gate_client:
+    async with HyperliquidClient(HYPERLIQUID_SECRET_KEY, HYPERLIQUID_ACCOUNT_ADDRESS) as hyperliquid_client:
+
+      gate: ExchangeClient = gate_client
+      hyperliquid: ExchangeClient = hyperliquid_client
+
+      open_gate = await gate.buy_market
+
+
+asyncio.run(main())
