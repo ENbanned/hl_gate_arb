5caa6c6c422fccf4a92e0247a0eb0470bb7337ee
ENbanned <your_email@example.com>
2025-10-23 19:55:01 +0200

    auto update: 2025-10-23 19:55:01

--- src/arbitrage/spread.py
+++ src/arbitrage/spread.py
@@ -5,19 +5,19 @@ from ..settings import GATE_TAKER_FEE, HYPERLIQUID_TAKER_FEE
 
 
 class SpreadFinder:
-  __slots__ = ('gate', 'hyperliquid', 'gate_taker_fee', 'hl_taker_fee')
+  __slots__ = ('gate', 'hyperliquid', 'gate_taker_fee', 'hyperliquid_taker_fee')
   
   def __init__(
     self,
     gate: ExchangeClient,
     hyperliquid: ExchangeClient,
     gate_taker_fee: Decimal = GATE_TAKER_FEE,
-    hl_taker_fee: Decimal = HYPERLIQUID_TAKER_FEE
+    hyperliquid_taker_fee: Decimal = HYPERLIQUID_TAKER_FEE
   ):
     self.gate = gate
     self.hyperliquid = hyperliquid
     self.gate_taker_fee = gate_taker_fee
-    self.hl_taker_fee = hl_taker_fee
+    self.hyperliquid_taker_fee = hyperliquid_taker_fee
 
 
   async def check_spread(self, symbol: str, size: float) -> list[dict]:
@@ -27,7 +27,19 @@ class SpreadFinder:
     hyperliquid_buy_price = await self.hyperliquid.estimate_fill_price(symbol, size, PositionSide.LONG)
     hyperliquid_sell_price = await self.hyperliquid.estimate_fill_price(symbol, size, PositionSide.SHORT)
 
+    # gate short -> hyperliquid long
+    spread_1 = (gate_sell_price - hyperliquid_buy_price) / hyperliquid_buy_price
+
+    # hyperliquid short -> gate long
+    spread_2 = (hyperliquid_sell_price - gate_buy_price) / gate_buy_price
+
+    total_fee = 2 * (GATE_TAKER_FEE + HYPERLIQUID_TAKER_FEE)
+
+    net_spread_1 = spread_1 - total_fee
+    net_spread_2 = spread_2 - total_fee
+
     
+
 
 
       
